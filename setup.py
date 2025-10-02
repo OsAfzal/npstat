86e854a15be873c7b9486d0da939a562c11b0471
OsAfzal <178735553+OsAfzal@users.noreply.github.com>
2025-10-02 11:32:24 +0500

    v0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="npstat",
-    version="0.3",
+    version="0.4",
     author="OsAfzal",
     description="Statistical hypothesis testing package",
     packages=find_packages(),
@@ -10,11 +10,14 @@ setup(
         "pandas",
         "scipy",
         "scikit-learn",
-        "matplotlib"
+        "matplotlib",
+        "seaborn",
+        "numpy"
     ],
     python_requires=">=3.6",
 
 )
+
 
 
 
