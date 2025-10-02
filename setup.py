8a36e74206d27aa94ca23b19253e26642c437b49
OsAfzal <178735553+OsAfzal@users.noreply.github.com>
2025-10-02 10:00:22 +0500

    v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="npstat",
-    version="0.2.1",
+    version="0.3",
     author="OsAfzal",
     description="Statistical hypothesis testing package",
     packages=find_packages(),
@@ -15,6 +15,7 @@ setup(
     python_requires=">=3.6",
 
 )
+
 
 
 
